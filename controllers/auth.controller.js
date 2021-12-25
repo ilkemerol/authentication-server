59710c1b8af4e2d8006c45882c9e8825b65aa228
İlkem Erol <ilkem.erol@obss.com.tr>
2021-12-25 13:41:30 +0300

    log added for remote pm2

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -98,6 +98,7 @@ exports.signin = (req, res) => {
       for (let i = 0; i < user.roles.length; i++) {
         authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
       }
+      console.log(user._id, "logging in system successfully.");
       res.status(200).send({
         id: user._id,
         username: user.username,
